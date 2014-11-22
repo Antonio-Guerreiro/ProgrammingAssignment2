9c3aaa42afc330bb6314550a1a366bdd9d53f90a
Antonio-Guerreiro <Antonio.Guerreiro@orange.fr>
2014-11-22 18:32:19 +0100

    Final cachematrix.R file
    Coursera’s R Programming cousre Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions show the use of the scoping rules of R
+## and of the <<- operator to cache the value of an object
+## rather than to recompute it.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function creates a special "matrix" object that can cache its inverse.
 
+makeCacheMatrix <- function(M = matrix()) {
+        i <- NULL
+        set <- function(N) {
+                M <<- N
+                i <<- NULL
+        }
+        get <- function() M
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" M
+## built by the makeCacheMatrix function above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve retrieves the inverse from the cache.
+## Assumes that the matrix supplied is invertible.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(M, ...) {
+        ## Return a matrix that is the inverse of the special 'matrix' M
+        i <- M$getinv()
+        if(!is.null(i)) {
+                message("getting cached data...")
+                return(i)
+        }
+        data <- M$get()
+        i <- solve(data)
+        M$setinv(i)
+        i
 }
